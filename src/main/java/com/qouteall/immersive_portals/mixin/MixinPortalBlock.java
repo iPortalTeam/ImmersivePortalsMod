0b224c4478a0b2c7c654fb3b37a293f92e27987c
qouteall <qouteall@outlook.com>
2019-07-30 12:05:11 +0800

    removed a overwrite

--- src/main/java/com/qouteall/immersive_portals/mixin/MixinPortalBlock.java
+++ src/main/java/com/qouteall/immersive_portals/mixin/MixinPortalBlock.java
@@ -4,15 +4,24 @@ import net.minecraft.block.PortalBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PortalBlock.class)
 public class MixinPortalBlock {
-    /**
-     * @author qouteall
-     */
-    @Overwrite
-    public boolean createPortalAt(IWorld iWorld_1, BlockPos blockPos_1) {
-        return false;
+    
+    @Inject(
+        method = "Lnet/minecraft/block/PortalBlock;createPortalAt(Lnet/minecraft/world/IWorld;Lnet/minecraft/util/math/BlockPos;)Z",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void onCreatePortal(
+        IWorld iWorld_1,
+        BlockPos blockPos_1,
+        CallbackInfoReturnable<Boolean> cir
+    ) {
+        cir.setReturnValue(false);
+        cir.cancel();
     }
 }
