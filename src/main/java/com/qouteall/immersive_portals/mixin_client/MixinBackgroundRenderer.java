38b85e84c4f9b91e4e48d985160572c22cc53190
qouteall <qouteall@outlook.com>
2020-04-18 13:23:55 +0800

    move mixins for alternate dimension

--- src/main/java/com/qouteall/immersive_portals/mixin_client/MixinBackgroundRenderer.java
+++ src/main/java/com/qouteall/immersive_portals/mixin_client/MixinBackgroundRenderer.java
@@ -1,16 +1,10 @@
 package com.qouteall.immersive_portals.mixin_client;
 
-import com.qouteall.hiding_in_the_bushes.alternate_dimension.AlternateDimension;
 import com.qouteall.immersive_portals.render.context_management.FogRendererContext;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BackgroundRenderer;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
@@ -27,27 +21,7 @@ public class MixinBackgroundRenderer {
     @Shadow
     private static long lastWaterFogColorUpdateTime = -1L;
     
-    //avoid alternate dimension dark when seeing from overworld
-    @Redirect(
-        method = "render",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/render/Camera;getPos()Lnet/minecraft/util/math/Vec3d;"
-        )
-    )
-    private static Vec3d redirectCameraGetPos(Camera camera) {
-        ClientWorld world = MinecraftClient.getInstance().world;
-        if (world != null && world.dimension instanceof AlternateDimension) {
-            return new Vec3d(
-                camera.getPos().x,
-                Math.max(32.0, camera.getPos().y),
-                camera.getPos().z
-            );
-        }
-        else {
-            return camera.getPos();
-        }
-    }
+   
     
     static {
         FogRendererContext.copyContextFromObject = context -> {
---
+++ src/main/java/com/qouteall/immersive_portals/mixin_client/alternate_dimension/MixinClientWorld_A.java
@@ -0,0 +1,45 @@
+package com.qouteall.immersive_portals.mixin_client.alternate_dimension;
+
+import com.qouteall.hiding_in_the_bushes.alternate_dimension.AlternateDimension;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientWorld.class)
+public class MixinClientWorld_A {
+    //avoid alternate dimension dark
+    @Inject(
+        method = "getSkyDarknessHeight",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void onGetSkyDarknessHeight(CallbackInfoReturnable<Double> cir) {
+        ClientWorld clientWorld = (ClientWorld) (Object) this;
+        if (clientWorld.dimension instanceof AlternateDimension) {
+            cir.setReturnValue(-100d);
+            cir.cancel();
+        }
+    }
+    
+    @Redirect(
+        method = "method_23777",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/biome/Biome;getSkyColor()I"
+        )
+    )
+    private int redirectBiomeGetSkyColor(Biome biome) {
+        ClientWorld this_ = (ClientWorld) ((Object) this);
+        if (this_.dimension instanceof AlternateDimension) {
+            return Biomes.PLAINS.getSkyColor();
+        }
+        else {
+            return biome.getSkyColor();
+        }
+    }
+}
