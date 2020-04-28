5e89d4e74bece063cf160c218e21257bb98a0331
Logan Darklock <logandarklock@gmail.com>
2020-04-28 06:36:37 -0700

    Make portal placeholder passthrough better

--- src/main/java/com/qouteall/immersive_portals/mixin/MixinRayTraceContext.java
+++ src/main/java/com/qouteall/immersive_portals/mixin/MixinRayTraceContext.java
@@ -1,10 +1,20 @@
 package com.qouteall.immersive_portals.mixin;
 
+import com.qouteall.immersive_portals.Global;
 import com.qouteall.immersive_portals.ducks.IERayTraceContext;
+import com.qouteall.immersive_portals.portal.PortalPlaceholderBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.RayTraceContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RayTraceContext.class)
 public abstract class MixinRayTraceContext implements IERayTraceContext {
@@ -26,5 +36,16 @@ public abstract class MixinRayTraceContext implements IERayTraceContext {
     public IERayTraceContext setEnd(Vec3d newEnd) {
         end = newEnd;
         return this;
+    }
+
+    @Inject(
+        at = @At("HEAD"),
+        method = "getBlockShape",
+        cancellable = true
+    )
+    private void onGetBlockShape(BlockState blockState, BlockView blockView, BlockPos blockPos, CallbackInfoReturnable<VoxelShape> cir) {
+        if (Global.portalPlaceholderPassthrough && blockState.getBlock() == PortalPlaceholderBlock.instance) {
+            cir.setReturnValue(VoxelShapes.empty());
+        }
     }
 }
