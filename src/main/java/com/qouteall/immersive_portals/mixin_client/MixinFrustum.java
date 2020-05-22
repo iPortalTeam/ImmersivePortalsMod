7d5dad3f4804b61fd259aba4d001473e0d3a0e63
qouteall <qouteall@163.com>
2020-05-22 11:48:45 +0800

    refactor frustum culling

--- src/main/java/com/qouteall/immersive_portals/mixin_client/MixinFrustum.java
+++ src/main/java/com/qouteall/immersive_portals/mixin_client/MixinFrustum.java
@@ -3,15 +3,12 @@ package com.qouteall.immersive_portals.mixin_client;
 import com.qouteall.immersive_portals.CGlobal;
 import com.qouteall.immersive_portals.render.FrustumCuller;
 import net.minecraft.client.render.Frustum;
-import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.function.Supplier;
 
 @Mixin(Frustum.class)
 public class MixinFrustum {
@@ -41,26 +38,14 @@ public class MixinFrustum {
         cancellable = true
     )
     private void onIntersectionTest(
-        double double_1,
-        double double_2,
-        double double_3,
-        double double_4,
-        double double_5,
-        double double_6,
+        double minX, double minY, double minZ, double maxX, double maxY, double maxZ,
         CallbackInfoReturnable<Boolean> cir
     ) {
         if (CGlobal.doUseAdvancedFrustumCulling) {
-            //this allocation should be avoided by jvm
-            Supplier<Box> boxInLocalCoordinateSupplier = () -> new Box(
-                double_1, double_2, double_3,
-                double_4, double_5, double_6
-            ).offset(
-                -x, -y, -z
-            );
-    
-            if (frustumCuller.canDetermineInvisible(boxInLocalCoordinateSupplier)) {
+            if (frustumCuller.canDetermineInvisible(
+                minX - x, minY - y, minZ - z, maxX - x, maxY - y, maxZ - z
+            )) {
                 cir.setReturnValue(false);
-                cir.cancel();
             }
         }
     }
