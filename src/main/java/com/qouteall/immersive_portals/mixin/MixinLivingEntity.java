88ed5f8acf2e2b1e5265e56d863ffbcdcf30c2cf
qouteall <qouteall@163.com>
2020-05-24 09:25:25 +0800

    fix enderman ai lag

---
+++ src/main/java/com/qouteall/immersive_portals/mixin/MixinLivingEntity.java
@@ -0,0 +1,22 @@
+package com.qouteall.immersive_portals.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class MixinLivingEntity {
+    @Inject(
+        method = "canSee",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void onCanSee(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (entity.dimension != ((Entity) (Object) this).dimension) {
+            cir.setReturnValue(false);
+        }
+    }
+}
--- src/main/java/com/qouteall/immersive_portals/mixin/block_manipulation/MixinLivingEntity_B.java
+++ src/main/java/com/qouteall/immersive_portals/mixin/block_manipulation/MixinLivingEntity_B.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity {
+public abstract class MixinLivingEntity_B {
     @Shadow public abstract AbstractEntityAttributeContainer getAttributes();
     
     @Inject(
