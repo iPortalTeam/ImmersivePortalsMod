d7652d16b80dd968c582011ddc1d374d1f8e4bbe
qouteall <qouteall@163.com>
2020-08-05 16:54:10 +0800

    freeze when the chunk is not loaded

--- src/main/java/com/qouteall/immersive_portals/mixin_client/MixinClientWorld.java
+++ src/main/java/com/qouteall/immersive_portals/mixin_client/MixinClientWorld.java
@@ -5,6 +5,7 @@ import com.qouteall.immersive_portals.CGlobal;
 import com.qouteall.immersive_portals.ducks.IEClientWorld;
 import com.qouteall.immersive_portals.portal.global_portals.GlobalTrackedPortal;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.world.ClientChunkManager;
 import net.minecraft.client.world.ClientWorld;
@@ -12,6 +13,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.chunk.EmptyChunk;
+import net.minecraft.world.chunk.WorldChunk;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,6 +24,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -60,7 +65,7 @@ public abstract class MixinClientWorld implements IEClientWorld {
     public void setGlobalPortals(List<GlobalTrackedPortal> arg) {
         globalTrackedPortals = arg;
     }
-    
+
 //    @Redirect(
 //        method = "<init>",
 //        at = @At(
@@ -71,7 +76,7 @@ public abstract class MixinClientWorld implements IEClientWorld {
 //    private ClientChunkManager replaceChunkManager(ClientWorld world, int loadDistance) {
 //        return O_O.createMyClientChunkManager(world, loadDistance);
 //    }
-
+    
     //use my client chunk manager
     @Inject(
         method = "<init>",
@@ -96,15 +101,30 @@ public abstract class MixinClientWorld implements IEClientWorld {
         chunkManager = myClientChunkManager;
     }
     
-    //avoid entity duplicate when an entity travels
+    // avoid entity duplicate when an entity travels
     @Inject(
         method = "addEntityPrivate",
         at = @At("TAIL")
     )
     private void onOnEntityAdded(int entityId, Entity entityIn, CallbackInfo ci) {
-        CGlobal.clientWorldLoader.clientWorldMap.values().stream()
-            .filter(world -> world != (Object) this)
-            .forEach(world -> world.removeEntity(entityId));
+        for (ClientWorld world : CGlobal.clientWorldLoader.clientWorldMap.values()) {
+            if (world != (Object) this) {
+                world.removeEntity(entityId);
+            }
+        }
+    }
+    
+    /**
+     * If the player goes into a portal when the other side chunk is not yet loaded
+     * freeze the player so the player won't drop
+     * {@link ClientPlayerEntity#tick()}
+     */
+    @Inject(method = "isChunkLoaded", at = @At("HEAD"), cancellable = true)
+    private void onIsChunkLoaded(int chunkX, int chunkZ, CallbackInfoReturnable<Boolean> cir) {
+        WorldChunk chunk = chunkManager.getChunk(chunkX, chunkZ, ChunkStatus.FULL, false);
+        if (chunk == null || chunk instanceof EmptyChunk) {
+            cir.setReturnValue(false);
+        }
     }
     
 }
