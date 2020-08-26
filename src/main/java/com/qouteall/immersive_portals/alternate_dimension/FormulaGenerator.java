822c1f95e12ea2354de8c5c2e8abf51060310f73
qouteall <qouteall@163.com>
2020-08-26 14:59:21 +0800

    overhaul the formula generator

--- src/main/java/com/qouteall/immersive_portals/alternate_dimension/FormulaGenerator.java
+++ src/main/java/com/qouteall/immersive_portals/alternate_dimension/FormulaGenerator.java
@@ -36,14 +36,44 @@ public class FormulaGenerator {
         builder.add(10, noRandomArg.apply(x -> Math.sin(x * 3)));
         builder.add(10, noRandomArg.apply(x -> Math.cos(x * 7)));
         builder.add(10, noRandomArg.apply(x -> Math.exp(x * 3)));
+        builder.add(10, noRandomArg.apply(x -> Math.log(Math.abs(x) + 1)));
+        builder.add(5, noRandomArg.apply(x -> Math.cosh(x)));
         builder.add(10, noRandomArg.apply(x -> Math.abs(x)));
+        builder.add(5, noRandomArg.apply(x -> Math.round(x)));
         builder.add(10, noRandomArg.apply(x -> Math.sqrt(Math.abs(x))));
+        builder.add(10, noRandomArg.apply(x -> weirdSwap(x)));
         builder.add(100, applyRandomArg.apply(arg -> x -> x * arg));
-        builder.add(50, applyRandomArg.apply(arg -> x -> x / Math.max(arg, 0.0001)));
+        builder.add(50, applyRandomArg.apply(arg -> x -> x / Math.max(arg, 0.001)));
         builder.add(10, applyRandomArg.apply(arg -> x -> Math.max(x, arg)));
         builder.add(10, applyRandomArg.apply(arg -> x -> Math.floor(x * arg * 23)));
+        builder.add(5, applyRandomArg.apply(arg -> x -> weirdAnd(arg, x)));
+        builder.add(10, applyRandomArg.apply(arg -> x -> weirdXor(arg, x)));
         
         uniFuncSelector = builder.build();
+    }
+    
+    private static int toInt(double d) {
+        return (int) (d * 256);
+    }
+    
+    private static double fromInt(int d) {
+        return (double) (((double) d) / 256);
+    }
+    
+    private static double weirdAnd(double a, double b) {
+        return fromInt(toInt(a) & toInt(b));
+    }
+    
+    private static double weirdXor(double a, double b) {
+        return fromInt(toInt(a) ^ toInt(b));
+    }
+    
+    private static double weirdSwap(double a) {
+        int i = toInt(a);
+        
+        int r = ((i & 0x0F) << 4) | ((i & 0xF0) >> 4);
+        
+        return fromInt(r);
     }
     
     private static void initTriFuncSelector() {
@@ -62,6 +92,10 @@ public class FormulaGenerator {
         builder.add(10, (x, y, z) -> -x * x + y * y + z * z);
         builder.add(10, (x, y, z) -> x * x - y * y + z * z);
         builder.add(10, (x, y, z) -> x * x + y * y - z * z);
+        builder.add(3, (x, y, z) -> Math.pow(y, x + z));
+        builder.add(3, (x, y, z) -> Math.pow(y, x - z));
+        builder.add(3, (x, y, z) -> Math.pow(x * z, y));
+        builder.add(20, (x, y, z) -> Math.log(Math.abs(x + y + z) + 0.5));
         
         triFuncSelector = builder.build();
     }
@@ -92,18 +126,40 @@ public class FormulaGenerator {
         return x -> base.eval(a.eval(x), b.eval(x), c.eval(x));
     }
     
-    public static UniNumFunction addConstant(
-        UniNumFunction a
+    public static UniNumFunction limitRange(
+        UniNumFunction fun
     ) {
-        double v1 = a.eval(0);
-        double v2 = a.eval(0.5);
-        double v3 = a.eval(1);
-        double average = (v1 + v2 + v3) / 3;
-        double sub = average - 0.5;
-        if (sub == 0) {
-            return a;
+        double v1 = fun.eval(0);
+        double v3 = fun.eval(1);
+        
+        final double tooBigThreshold = 233.0;
+        // having a too big number range may cause it to generate solid or void
+        if (Math.abs(v1) > tooBigThreshold || Math.abs(v3) > tooBigThreshold) {
+            return x -> Math.log(Math.abs(fun.eval(x)) + 1);
         }
-        return x -> a.eval(x) - sub;
+        else {
+            return fun;
+        }
+    }
+    
+    public static UniNumFunction addConstant(
+        UniNumFunction fun
+    ) {
+        double v1 = fun.eval(0);
+        double v2 = fun.eval(0.5);
+        double v3 = fun.eval(1);
+        
+        double average = (v1 + v2 + v3) / 3;
+        double offset = 0.5 - average;
+        if (Math.abs(offset) < 0.1) {
+            return fun;
+        }
+        
+        return x -> fun.eval(x) + offset;
+    }
+    
+    public static UniNumFunction processFunc(UniNumFunction fun) {
+        return addConstant(limitRange(fun));
     }
     
     public static UniNumFunction getRandomUniExpressionOld(
@@ -185,52 +241,52 @@ public class FormulaGenerator {
             return (x, y, z) -> x * y * z;
         }
     }
-    
-    public static UniNumFunction getComplexUniExpressionOld(
-        Random random,
-        int nestingLayer
-    ) {
-        if (nestingLayer == 0) {
-            return addConstant(
-                mergeExpression(
-                    getRandomTriExpressionOld(random.nextInt()),
-                    addConstant(getRandomUniExpressionOld(random.nextInt())),
-                    addConstant(getRandomUniExpressionOld(random.nextInt())),
-                    addConstant(getRandomUniExpressionOld(random.nextInt()))
-                )
-            );
-        }
-        
-        return mergeExpression(
-            getRandomTriExpressionOld(random.nextInt()),
-            getComplexUniExpressionOld(random, nestingLayer - 1),
-            getComplexUniExpressionOld(random, nestingLayer - 1),
-            getComplexUniExpressionOld(random, nestingLayer - 1)
-        );
-    }
-    
-    public static TriNumFunction getRandomTriCompositeExpressionOld(
-        Random random
-    ) {
-        return nestExpression(
-            getRandomTriExpressionOld(random.nextInt()),
-            getComplexUniExpressionOld(random, 2),
-            getComplexUniExpressionOld(random, 3),
-            getComplexUniExpressionOld(random, 2)
-        );
-    }
+
+//    public static UniNumFunction getComplexUniExpressionOld(
+//        Random random,
+//        int nestingLayer
+//    ) {
+//        if (nestingLayer == 0) {
+//            return addConstant(
+//                mergeExpression(
+//                    getRandomTriExpressionOld(random.nextInt()),
+//                    addConstant(getRandomUniExpressionOld(random.nextInt())),
+//                    addConstant(getRandomUniExpressionOld(random.nextInt())),
+//                    addConstant(getRandomUniExpressionOld(random.nextInt()))
+//                )
+//            );
+//        }
+//
+//        return mergeExpression(
+//            getRandomTriExpressionOld(random.nextInt()),
+//            getComplexUniExpressionOld(random, nestingLayer - 1),
+//            getComplexUniExpressionOld(random, nestingLayer - 1),
+//            getComplexUniExpressionOld(random, nestingLayer - 1)
+//        );
+//    }
+
+//    public static TriNumFunction getRandomTriCompositeExpressionOld(
+//        Random random
+//    ) {
+//        return nestExpression(
+//            getRandomTriExpressionOld(random.nextInt()),
+//            getComplexUniExpressionOld(random, 2),
+//            getComplexUniExpressionOld(random, 3),
+//            getComplexUniExpressionOld(random, 2)
+//        );
+//    }
     
     public static UniNumFunction getComplexUniExpression(
         Random random,
         int nestingLayer
     ) {
         if (nestingLayer == 0) {
-            return addConstant(
+            return processFunc(
                 mergeExpression(
                     triFuncSelector.select(random),
-                    addConstant(uniFuncSelector.select(random).apply(random)),
-                    addConstant(uniFuncSelector.select(random).apply(random)),
-                    addConstant(uniFuncSelector.select(random).apply(random))
+                    processFunc(uniFuncSelector.select(random).apply(random)),
+                    processFunc(uniFuncSelector.select(random).apply(random)),
+                    processFunc(uniFuncSelector.select(random).apply(random))
                 )
             );
         }
