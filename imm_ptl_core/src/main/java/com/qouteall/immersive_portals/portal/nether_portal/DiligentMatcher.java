23c878c8139c0bf3a10e2a9c47736a8b34357094
qouteall <qouteall@163.com>
2020-08-28 23:12:30 +0800

    Add DiligentMatcher

--- imm_ptl_core/src/main/java/com/qouteall/immersive_portals/portal/nether_portal/DiligentMatcher.java
+++ imm_ptl_core/src/main/java/com/qouteall/immersive_portals/portal/nether_portal/DiligentMatcher.java
@@ -17,6 +17,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -48,47 +49,175 @@ public class DiligentMatcher {
             return r > 0;
         }
         
+        // p * m  p is horizontal vector
         public BlockPos transform(Vec3i p) {
             return Helper.scale(x, p.getX())
                 .add(Helper.scale(y, p.getY()))
                 .add(Helper.scale(z, p.getZ()));
         }
         
+        public IntMatrix3 multiply(IntMatrix3 m) {
+            return new IntMatrix3(
+                m.transform(x),
+                m.transform(y),
+                m.transform(z)
+            );
+        }
+        
         public Direction transformDirection(Direction direction) {
             BlockPos vec = transform(direction.getVector());
             return Direction.fromVector(vec.getX(), vec.getY(), vec.getZ());
         }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            IntMatrix3 that = (IntMatrix3) o;
+            return x.equals(that.x) &&
+                y.equals(that.y) &&
+                z.equals(that.z);
+        }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y, z);
+        }
+        
+        public static IntMatrix3 getIdentity() {
+            return new IntMatrix3(
+                new BlockPos(1, 0, 0),
+                new BlockPos(0, 1, 0),
+                new BlockPos(0, 0, 1)
+            );
+        }
     }
     
+    public static int dirDotProduct(Direction dir, Direction b) {
+        if (dir.getAxis() != b.getAxis()) {
+            return 0;
+        }
+        if (dir.getDirection() == b.getDirection()) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+    
+    // counter clockwise
+    public static Direction rotateAlong(Direction dir, Direction axis) {
+        Pair<Direction, Direction> ds = Helper.getPerpendicularDirections(axis);
+        Direction d1 = ds.getLeft();
+        Direction d2 = ds.getRight();
+        
+        int c1 = dirDotProduct(dir, d1);
+        int c2 = dirDotProduct(dir, d2);
+        int ca = dirDotProduct(dir, axis);
+        
+        int nc1 = -c2;
+        int nc2 = c1;
+        
+        BlockPos finalVec = Helper.scale(d1.getVector(), nc1)
+            .add(Helper.scale(d2.getVector(), nc2))
+            .add(Helper.scale(axis.getVector(), ca));
+        return Direction.fromVector(finalVec.getX(), finalVec.getY(), finalVec.getZ());
+    }
+    
+    public static IntMatrix3 getRotation90(Direction direction) {
+        return new IntMatrix3(
+            rotateAlong(Direction.from(Direction.Axis.X, Direction.AxisDirection.POSITIVE), direction).getVector(),
+            rotateAlong(Direction.from(Direction.Axis.Y, Direction.AxisDirection.POSITIVE), direction).getVector(),
+            rotateAlong(Direction.from(Direction.Axis.Z, Direction.AxisDirection.POSITIVE), direction).getVector()
+        );
+    }
+    
+    // sorted from simpler rotations to complex rotations
     public static final List<IntMatrix3> rotationTransformations = Util.make(() -> {
-        return Arrays.stream(DirectionTransformation.values())
-            .map(IntMatrix3::new)
-            .filter(IntMatrix3::isRotationTransformation)
+        List<IntMatrix3> basicRotations = Arrays.stream(Direction.values())
+            .map(DiligentMatcher::getRotation90)
             .collect(Collectors.toList());
+        
+        IntMatrix3 identity = IntMatrix3.getIdentity();
+        
+        ArrayList<IntMatrix3> rotationList = new ArrayList<>();
+        Set<IntMatrix3> rotationSet = new HashSet<>();
+        
+        rotationList.add(identity);
+        rotationSet.add(identity);
+        
+        for (int i = 0; i < 3; i++) {
+            ArrayList<IntMatrix3> newlyAdded = new ArrayList<>(rotationList);
+            for (IntMatrix3 rot : rotationList) {
+                for (IntMatrix3 basicRotation : basicRotations) {
+                    IntMatrix3 newRot = rot.multiply(basicRotation);
+                    if (!rotationSet.contains(newRot)) {
+                        rotationSet.add(newRot);
+                        newlyAdded.add(newRot);
+                    }
+                }
+            }
+            
+            rotationList.addAll(newlyAdded);
+        }
+        
+        Validate.isTrue(rotationList.size() == 24);
+        
+        return rotationList;
     });
     
-    public static List<BlockPortalShape> getMatchableShapeVariants(
+    public static class TransformedShape {
+        public final BlockPortalShape originalShape;
+        public final BlockPortalShape transformedShape;
+        public final IntMatrix3 rotation;
+        public final double scale;
+        
+        public TransformedShape(BlockPortalShape originalShape, BlockPortalShape transformedShape, IntMatrix3 rotation, double scale) {
+            this.originalShape = originalShape;
+            this.transformedShape = transformedShape;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+    }
+    
+    public static List<TransformedShape> getMatchableShapeVariants(
         BlockPortalShape original,
         int maxShapeLen
     ) {
-        HashSet<BlockPortalShape> result = new HashSet<>();
+        List<TransformedShape> result = new ArrayList<>();
+        HashSet<BlockPortalShape> shapeSet = new HashSet<>();
         
-        BlockPortalShape shrinked = shrinkShape(original);
+        int divFactor = getShapeShrinkFactor(original);
+        
+        BlockPortalShape shrinked = shrinkShapeBy(original, divFactor);
         
         BlockPos shrinkedShapeSize = shrinked.innerAreaBox.getSize();
         int shrinkedShapeLen = Math.max(shrinkedShapeSize.getX(), Math.max(shrinkedShapeSize.getY(), shrinkedShapeSize.getZ()));
-        int multiplyFactor = (int) Math.ceil(((double) maxShapeLen) / shrinkedShapeLen);
+        int maxMultiplyFactor = (int) Math.ceil(((double) maxShapeLen) / shrinkedShapeLen);
         
-        for (IntMatrix3 t : rotationTransformations) {
-            BlockPortalShape rotatedShape = rotateShape(shrinked, t);
-            result.add(regularizeShape(rotatedShape));
+        for (IntMatrix3 rotation : rotationTransformations) {
+            BlockPortalShape rotatedShape = rotateShape(shrinked, rotation);
+            BlockPortalShape newShape = regularizeShape(rotatedShape);
+            boolean isNew = shapeSet.add(newShape);
+            if (isNew) {
+                result.add(new TransformedShape(
+                    original, newShape, rotation, 1.0 / divFactor
+                ));
+            }
             
-            for (int mul = 2; mul <= multiplyFactor; mul++) {
-                result.add(regularizeShape(expandShape(rotatedShape, mul)));
+            for (int mul = 2; mul <= maxMultiplyFactor; mul++) {
+                BlockPortalShape expanded = regularizeShape(expandShape(rotatedShape, mul));
+                isNew = shapeSet.add(expanded);
+                if (isNew) {
+                    result.add(new TransformedShape(
+                        original, expanded,
+                        rotation, ((double) mul) / divFactor
+                    ));
+                }
             }
         }
         
-        return new ArrayList<>(result);
+        return result;
     }
     
     public static BlockPortalShape regularizeShape(BlockPortalShape rotatedShape) {
@@ -105,11 +234,11 @@ public class DiligentMatcher {
         return new BlockPortalShape(newArea, newAxis);
     }
     
-    public static BlockPortalShape shrinkShape(BlockPortalShape shape) {
+    public static int getShapeShrinkFactor(BlockPortalShape shape) {
         HashSet<BlockPos> area = new HashSet<>(shape.area);
-    
+        
         ArrayList<IntBox> boxList = decomposeShape(shape, area);
-    
+        
         IntArrayList sideLenList = new IntArrayList();
         Pair<Direction.Axis, Direction.Axis> axs = Helper.getAnotherTwoAxis(shape.axis);
         for (IntBox box : boxList) {
@@ -120,8 +249,10 @@ public class DiligentMatcher {
             sideLenList.add(b);
         }
         
-        int div = sideLenList.stream().reduce(DiligentMatcher::getGreatestCommonDivisor).get();
-        
+        return sideLenList.stream().reduce(DiligentMatcher::getGreatestCommonDivisor).get();
+    }
+    
+    public static BlockPortalShape shrinkShapeBy(BlockPortalShape shape, int div) {
         Validate.isTrue(div != 0);
         
         if (div == 1) {
