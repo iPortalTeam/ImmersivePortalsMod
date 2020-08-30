911442c39b12c2345a12194ec1bc19ecad8e4f54
qouteall <qouteall@163.com>
2020-08-30 20:06:30 +0800

    Move the remove boss fog mixin into peripheral

--- src/main/java/com/qouteall/imm_ptl_peripheral/mixin/client/MixinBossBarHud.java
+++ src/main/java/com/qouteall/imm_ptl_peripheral/mixin/client/MixinBossBarHud.java
@@ -1,4 +1,4 @@
-package com.qouteall.immersive_portals.mixin.client;
+package com.qouteall.imm_ptl_peripheral.mixin.client;
 
 import net.minecraft.client.gui.hud.BossBarHud;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,6 +8,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BossBarHud.class)
 public class MixinBossBarHud {
+    // the boss info does not get synced through portal
+    // so when the player jumps into end portal the fog will abruptly become thick
+    // avoid thicken fog to make the teleportation seamless
     @Inject(method = "shouldThickenFog", at = @At("HEAD"), cancellable = true)
     private void onShouldThickenFog(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
