1f535b577794ec8a380dc64255b834047b767529
qouteall <qouteall@163.com>
2020-09-01 12:45:20 +0800

    fix optifine things

--- imm_ptl_core/src/main/java/com/qouteall/hiding_in_the_bushes/LaunchMixinWithOptifineRunnable.java
+++ imm_ptl_core/src/main/java/com/qouteall/hiding_in_the_bushes/LaunchMixinWithOptifineRunnable.java
@@ -1,15 +1,13 @@
 package com.qouteall.hiding_in_the_bushes;
 
-import com.qouteall.immersive_portals.Helper;
 import net.fabricmc.loader.FabricLoader;
 import org.spongepowered.asm.mixin.Mixins;
 
 public class LaunchMixinWithOptifineRunnable implements Runnable {
     @Override
     public void run() {
-        Helper.log("oops");
         if (FabricLoader.INSTANCE.isModLoaded("optifabric")) {
-            Helper.log("Registering Mixin for OptiFine");
+            System.out.println("Registering Mixin for OptiFine");//Helper.log may load classes
             Mixins.addConfiguration("imm_ptl_optifine.mixins.json");
         }
     }
