8531764dbec2698b521e1202c817cfc043dc190a
qouteall <qouteall@163.com>
2020-09-17 18:48:22 +0800

    adaptive type nether portal generates nether portal

--- src/main/java/com/qouteall/imm_ptl_peripheral/portal_generation/DiligentNetherPortalForm.java
+++ src/main/java/com/qouteall/imm_ptl_peripheral/portal_generation/DiligentNetherPortalForm.java
@@ -2,9 +2,12 @@ package com.qouteall.imm_ptl_peripheral.portal_generation;
 
 import com.mojang.serialization.Codec;
 import com.qouteall.hiding_in_the_bushes.O_O;
+import com.qouteall.immersive_portals.portal.custom_portal_gen.PortalGenInfo;
 import com.qouteall.immersive_portals.portal.custom_portal_gen.form.AbstractDiligentForm;
 import com.qouteall.immersive_portals.portal.custom_portal_gen.form.PortalGenForm;
 import com.qouteall.immersive_portals.portal.nether_portal.BlockPortalShape;
+import com.qouteall.immersive_portals.portal.nether_portal.BreakablePortalEntity;
+import com.qouteall.immersive_portals.portal.nether_portal.NetherPortalEntity;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -23,6 +26,12 @@ public class DiligentNetherPortalForm extends AbstractDiligentForm {
         for (BlockPos blockPos : toShape.frameAreaWithCorner) {
             toWorld.setBlockState(blockPos, Blocks.OBSIDIAN.getDefaultState());
         }
+    }
+    
+    @Override
+    public BreakablePortalEntity[] generatePortalEntitiesAndPlaceholder(PortalGenInfo info) {
+        info.generatePlaceholderBlocks();
+        return info.generateBiWayBiFacedPortal(NetherPortalEntity.entityType);
     }
     
     @Override
