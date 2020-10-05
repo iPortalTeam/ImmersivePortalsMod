2b940cb6b4cbb120f88c34dd26ace4637fdce92c
qouteall <qouteall@163.com>
2020-10-05 20:06:47 +0800

    remove languagehack

--- src/main/java/com/qouteall/hiding_in_the_bushes/PeripheralModEntry.java
+++ src/main/java/com/qouteall/hiding_in_the_bushes/PeripheralModEntry.java
@@ -1,17 +1,12 @@
 package com.qouteall.hiding_in_the_bushes;
 
 import com.qouteall.imm_ptl_peripheral.PeripheralModMain;
-import com.qouteall.imm_ptl_peripheral.alternate_dimension.FormulaGenerator;
 import net.fabricmc.api.ModInitializer;
-import net.logandark.languagehack.LanguageHack;
 
 public class PeripheralModEntry implements ModInitializer {
     @Override
     public void onInitialize() {
-        LanguageHack.activate("immersive_portals");
-    
-        FormulaGenerator.init();
-    
+        
         PeripheralModMain.init();
     }
 }
--- src/main/java/com/qouteall/imm_ptl_peripheral/PeripheralModMain.java
+++ src/main/java/com/qouteall/imm_ptl_peripheral/PeripheralModMain.java
@@ -1,5 +1,6 @@
 package com.qouteall.imm_ptl_peripheral;
 
+import com.qouteall.imm_ptl_peripheral.alternate_dimension.FormulaGenerator;
 import com.qouteall.imm_ptl_peripheral.altius_world.AltiusGameRule;
 import com.qouteall.imm_ptl_peripheral.altius_world.AltiusManagement;
 import com.qouteall.imm_ptl_peripheral.portal_generation.IntrinsicPortalGeneration;
@@ -8,6 +9,8 @@ public class PeripheralModMain {
     
     
     public static void init() {
+        FormulaGenerator.init();
+        
         IntrinsicPortalGeneration.init();
     
         AltiusGameRule.init();
