d949d59180d355202a31996968fa6902deba109f
qouteall <qouteall@163.com>
2020-10-25 21:36:28 +0800

    fix for forge version

--- imm_ptl_core/src/main/java/com/qouteall/immersive_portals/mixin/common/MixinServerWorld.java
+++ imm_ptl_core/src/main/java/com/qouteall/immersive_portals/mixin/common/MixinServerWorld.java
@@ -53,7 +53,8 @@ public abstract class MixinServerWorld implements IEServerWorld {
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/Packet;)V"
-        )
+        ),
+        require = 0 //Forge changes that. avoid crashing in forge version
     )
     private void redirectSendToAll(PlayerManager playerManager, Packet<?> packet) {
         final ServerWorld this_ = (ServerWorld) (Object) this;
