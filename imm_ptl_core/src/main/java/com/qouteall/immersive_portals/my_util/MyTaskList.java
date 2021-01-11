75b503f0abbc3bc4147706546c45715f4594a44b
qouteall <qouteall@163.com>
2021-01-11 15:32:50 +0800

    add some task composition methods

--- imm_ptl_core/src/main/java/com/qouteall/immersive_portals/my_util/MyTaskList.java
+++ imm_ptl_core/src/main/java/com/qouteall/immersive_portals/my_util/MyTaskList.java
@@ -4,11 +4,13 @@ import com.qouteall.immersive_portals.Helper;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectList;
 
+import java.util.function.BooleanSupplier;
+
 //NOTE if the task returns true, it will be deleted
 //if the task returns false, it will be invoked again at next time
 public class MyTaskList {
     public interface MyTask {
-        public boolean runAndGetIsSucceeded();
+        public boolean runAndGetIsFinished();
     }
     
     private final ObjectList<MyTask> tasks = new ObjectArrayList<>();
@@ -25,7 +27,7 @@ public class MyTaskList {
         
         Helper.removeIf(tasks, task -> {
             try {
-                return task.runAndGetIsSucceeded();
+                return task.runAndGetIsFinished();
             }
             catch (Throwable e) {
                 Helper.err("Failed to process task " + task);
@@ -38,5 +40,118 @@ public class MyTaskList {
     public synchronized void forceClearTasks() {
         tasks.clear();
         tasksToAdd.clear();
+    }
+    
+    public static MyTask oneShotTask(Runnable runnable) {
+        return () -> {
+            runnable.run();
+            return true;
+        };
+    }
+    
+    public static MyTask withDelay(int iterations, MyTask task) {
+        return new MyTask() {
+            private int counter = iterations;
+            
+            @Override
+            public boolean runAndGetIsFinished() {
+                if (counter > 0) {
+                    counter--;
+                    return false;
+                }
+                else {
+                    return task.runAndGetIsFinished();
+                }
+            }
+        };
+    }
+    
+    public static MyTask withCancelCondition(BooleanSupplier shouldCancel, MyTask task) {
+        return () -> {
+            if (shouldCancel.getAsBoolean()) {
+                return true;
+            }
+            
+            return task.runAndGetIsFinished();
+        };
+    }
+    
+    public static MyTask withRetryCondition(BooleanSupplier shouldRetry, MyTask task) {
+        return () -> {
+            if (shouldRetry.getAsBoolean()) {
+                return false;
+            }
+            
+            return task.runAndGetIsFinished();
+        };
+    }
+    
+    public static MyTask withRetryNumberLimit(int retryNumberLimit, MyTask task) {
+        return new MyTask() {
+            private int retryNumber = 0;
+            
+            @Override
+            public boolean runAndGetIsFinished() {
+                boolean finished = task.runAndGetIsFinished();
+                if (finished) {
+                    return true;
+                }
+                else {
+                    retryNumber += 1;
+                    if (retryNumber > retryNumberLimit) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        };
+    }
+    
+    public static MyTask withInterval(int interval, MyTask task) {
+        return new MyTask() {
+            int i = 0;
+            
+            @Override
+            public boolean runAndGetIsFinished() {
+                if (i < interval) {
+                    i++;
+                    return false;
+                }
+                i = 0;
+                return task.runAndGetIsFinished();
+            }
+        };
+    }
+    
+    public static MyTask withMacroLifecycle(
+        Runnable beginAction, Runnable endAction, MyTask task
+    ) {
+        return new MyTask() {
+            boolean began = false;
+            
+            @Override
+            public boolean runAndGetIsFinished() {
+                if (!began) {
+                    began = true;
+                    beginAction.run();
+                }
+                boolean finished = task.runAndGetIsFinished();
+                if (finished) {
+                    endAction.run();
+                }
+                return finished;
+            }
+        };
+    }
+    
+    public static MyTask withMicroLifecycle(
+        Runnable beginAction, Runnable endAction, MyTask task
+    ) {
+        return () -> {
+            beginAction.run();
+            boolean finished = task.runAndGetIsFinished();
+            endAction.run();
+            return finished;
+        };
     }
 }
