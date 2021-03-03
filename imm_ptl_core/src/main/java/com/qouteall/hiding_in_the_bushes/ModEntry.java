fbfa8b52e8d5b283a8fd432b56094a9252b200b6
qouteall <qouteall@163.com>
2021-03-03 12:27:36 +0800

    add test

--- imm_ptl_core/src/main/java/com/qouteall/hiding_in_the_bushes/ModEntry.java
+++ imm_ptl_core/src/main/java/com/qouteall/hiding_in_the_bushes/ModEntry.java
@@ -3,8 +3,16 @@ package com.qouteall.hiding_in_the_bushes;
 import com.qouteall.immersive_portals.Helper;
 import com.qouteall.immersive_portals.ModMain;
 import com.qouteall.immersive_portals.PehkuiInterface;
+import com.qouteall.immersive_portals.api.PortalAPI;
+import com.qouteall.immersive_portals.chunk_loading.ChunkLoader;
+import com.qouteall.immersive_portals.chunk_loading.DimensionalChunkPos;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
+
+import java.util.WeakHashMap;
 
 public class ModEntry implements ModInitializer {
     
@@ -40,7 +48,25 @@ public class ModEntry implements ModInitializer {
             Helper.log("Pehkui is not present");
         }
         
-        
+        test();
+    }
+    
+    private static final WeakHashMap<ServerPlayerEntity, ChunkLoader> chunkLoaderMap =
+        new WeakHashMap<>();
+    
+    private static void test() {
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (entity instanceof ServerPlayerEntity) {
+                ServerPlayerEntity player = (ServerPlayerEntity) entity;
+                if (!chunkLoaderMap.containsKey(player)) {
+                    ChunkLoader chunkLoader = new ChunkLoader(
+                        new DimensionalChunkPos(World.END, 100, 100), 2
+                    );
+                    chunkLoaderMap.put(player, chunkLoader);
+                    PortalAPI.addChunkLoaderForPlayer(player, chunkLoader);
+                }
+            }
+        });
     }
     
 }
