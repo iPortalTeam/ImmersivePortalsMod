7e5cc6458cc0f8d0604be845e99db1a666aa0b6b
qouteall <qouteall@163.com>
2021-03-11 10:07:38 +0800

    minor optimization

--- imm_ptl_core/src/main/java/com/qouteall/immersive_portals/render/PortalRenderer.java
+++ imm_ptl_core/src/main/java/com/qouteall/immersive_portals/render/PortalRenderer.java
@@ -134,13 +134,14 @@ public abstract class PortalRenderer {
             }
         }
         
-        if (isOutOfDistance(portal)) {
+        double distance = portal.getDistanceToNearestPointInPortal(cameraPos);
+        if (distance > getRenderRange()) {
             return true;
         }
         
         if (CGlobal.earlyFrustumCullingPortal) {
             // frustum culling does not work when portal is very close
-            if (portal.getDistanceToNearestPointInPortal(cameraPos) > 0.03) {
+            if (distance > 0.03) {
                 Frustum frustum = frustumSupplier.get();
                 if (!frustum.isVisible(portal.getExactAreaBox())) {
                     return true;
@@ -230,16 +231,6 @@ public abstract class PortalRenderer {
             worldRenderInfo,
             Runnable::run
         );
-    }
-    
-    private boolean isOutOfDistance(PortalLike portal) {
-        
-        Vec3d cameraPos = CHelper.getCurrentCameraPos();
-        if (portal.getDistanceToNearestPointInPortal(cameraPos) > getRenderRange()) {
-            return true;
-        }
-        
-        return false;
     }
     
     @Nullable
