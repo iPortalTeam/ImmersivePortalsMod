6ad25ba5ab1b7208031bfe8ad04cf4b86bdc6795
qouteall <qouteall@163.com>
2021-06-06 23:07:34 +0800

    fix dedicated server resource loading

--- q_misc_util/src/main/java/qouteall/q_misc_util/mixin/MixinMinecraftServer_Misc.java
+++ q_misc_util/src/main/java/qouteall/q_misc_util/mixin/MixinMinecraftServer_Misc.java
@@ -1,27 +1,36 @@
 package qouteall.q_misc_util.mixin;
 
+import com.google.gson.JsonElement;
 import com.mojang.authlib.GameProfileRepository;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.datafixers.DataFixer;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.resource.ServerResourceManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.util.UserCache;
+import net.minecraft.util.dynamic.RegistryOps;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.world.SaveProperties;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import qouteall.q_misc_util.Helper;
 import qouteall.q_misc_util.MiscHelper;
 
 import java.lang.ref.WeakReference;
 import java.net.Proxy;
 
 @Mixin(MinecraftServer.class)
-public class MixinMinecraftServer_Misc {
+public abstract class MixinMinecraftServer_Misc {
+    @Shadow
+    public abstract boolean isDedicated();
+    
     @Inject(
         method = "<init>",
         at = @At("RETURN")
@@ -36,5 +45,22 @@ public class MixinMinecraftServer_Misc {
         CallbackInfo ci
     ) {
         MiscHelper.refMinecraftServer = new WeakReference<>((MinecraftServer) ((Object) this));
+        
+        if (isDedicated()) {
+            loadResourcesIntoRegistry(registryManager, serverResourceManager);
+        }
+    }
+    
+    private void loadResourcesIntoRegistry(
+        DynamicRegistryManager registryTracker, ServerResourceManager resourceManager
+    ) {
+        Helper.log(
+            "Reloading Server Resources to Ensure Custom Dimension Type Loading." +
+                " This may cause other issues"
+        );
+        RegistryOps<JsonElement> registryOps =
+            RegistryOps.of(JsonOps.INSTANCE, resourceManager.getResourceManager(), registryTracker);
+        
+        DynamicRegistryManager.load(registryTracker, registryOps);
     }
 }
