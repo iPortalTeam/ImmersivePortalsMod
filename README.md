9cf86cf47744f8a74b1f29625786f05ce893aaf7
qouteall <qouteall@163.com>
2021-07-18 14:20:00 +0800

    change splash text

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Immersive Portals Mod
 
 It's a minecraft mod that provides see-through portals and seamless teleportation.
-It allows creating "Uneuclidean" space effect.
+It allows creating "Non-Euclidean" (Uneuclidean) space effect.
 
 https://www.curseforge.com/minecraft/mc-mods/immersive-portals-mod
 
---
+++ src/main/java/qouteall/imm_ptl/peripheral/mixin/client/MixinSplashTextResourceSupplier.java
@@ -0,0 +1,32 @@
+package qouteall.imm_ptl.peripheral.mixin.client;
+
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(SplashTextResourceSupplier.class)
+public class MixinSplashTextResourceSupplier {
+    @Shadow
+    @Final
+    private List<String> splashTexts;
+    
+    @Inject(method = "apply", at = @At("RETURN"))
+    private void onApply(
+        List<String> list, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci
+    ) {
+        splashTexts.remove("Euclidian!");
+        splashTexts.remove("Slow acting portals!");
+        splashTexts.add("Non-Euclidean!");
+        splashTexts.add("Fast acting portals!");
+        splashTexts.add("Immersive Portals!");
+    }
+}
