2b296eab6903616c6b88a9812df9503a6e453118
qouteall <qouteall@163.com>
2022-06-22 17:36:52 +0800

    Drop leash when the player goes to another dimension

---
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/mixin/common/miscellaneous/MixinMob.java
@@ -0,0 +1,34 @@
+package qouteall.imm_ptl.core.mixin.common.miscellaneous;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mob.class)
+public abstract class MixinMob {
+    @Shadow
+    private @Nullable Entity leashHolder;
+    
+    @Shadow
+    public abstract void dropLeash(boolean sendPacket, boolean dropLead);
+    
+    // drop leash when the player goes to another dimension
+    @Inject(
+        method = "tickLeash",
+        at = @At("RETURN")
+    )
+    private void onTickLeash(CallbackInfo ci) {
+        Mob this_ = (Mob) (Object) this;
+        
+        if (leashHolder != null) {
+            if (leashHolder.level != this_.level) {
+                dropLeash(true, true);
+            }
+        }
+    }
+}
