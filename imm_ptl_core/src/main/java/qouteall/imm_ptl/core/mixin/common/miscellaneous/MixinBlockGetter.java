5294a0b3d77f86ceeaf766eae9eeb4998e71f862
qouteall <qouteall@163.com>
2022-06-29 14:47:35 +0800

    add error logging for too far raycast

---
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/mixin/common/miscellaneous/MixinBlockGetter.java
@@ -0,0 +1,40 @@
+package qouteall.imm_ptl.core.mixin.common.miscellaneous;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import qouteall.imm_ptl.core.IPMcHelper;
+import qouteall.q_misc_util.Helper;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+@Mixin(BlockGetter.class)
+public interface MixinBlockGetter {
+	
+	@ModifyVariable(
+		method = "traverseBlocks",
+		at = @At("HEAD"),
+		argsOnly = true,
+		index = 1
+	)
+	private static <T, C> Vec3 onTraverseBlocks(
+		Vec3 originalArgument,
+		Vec3 from, Vec3 _to, C context,
+		BiFunction<C, BlockPos, T> tester, Function<C, T> onFail
+	) {
+		if (from.distanceToSqr(_to) > (256 * 256)) {
+			IPMcHelper.limitedLogger.invoke(() -> {
+				Helper.err("raycast too far");
+				new Throwable().printStackTrace();
+			});
+			return _to.subtract(from).normalize().scale(30).add(from);
+		}
+		return _to;
+	}
+	
+}
