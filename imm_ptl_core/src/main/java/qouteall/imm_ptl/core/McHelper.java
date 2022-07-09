abf5699494df2f8c9f0936ba9b6c69cfda4e3059
qouteall <qouteall@163.com>
2022-07-09 21:37:52 +0800

    probably fix crash due to too large scale from pehkui

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/McHelper.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/McHelper.java
@@ -136,8 +136,7 @@ public class McHelper {
                     else {
                         finishBehavior.obj = onNotFound;
                     }
-                }
-                catch (Throwable t) {
+                } catch (Throwable t) {
                     t.printStackTrace();
                     finishBehavior.obj = () -> {
                         t.printStackTrace();
@@ -499,7 +498,6 @@ public class McHelper {
         );
     }
     
-    //faster
     public static <T extends Entity> List<T> findEntitiesRough(
         Class<T> entityClass,
         Level world,
@@ -507,9 +505,13 @@ public class McHelper {
         int radiusChunks,
         Predicate<T> predicate
     ) {
-        // the minimun is 1
-        if (radiusChunks == 0) {
+        // the minimum is 1
+        if (radiusChunks <= 0) {
             radiusChunks = 1;
+        }
+        
+        if (radiusChunks > 32) {
+            radiusChunks = 32;
         }
         
         ChunkPos chunkPos = new ChunkPos(new BlockPos(center));
@@ -762,8 +764,7 @@ public class McHelper {
                 ).get().open();
             
             result = IOUtils.toString(inputStream, Charset.defaultCharset());
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException("Error loading " + identifier, e);
         }
         return result;
