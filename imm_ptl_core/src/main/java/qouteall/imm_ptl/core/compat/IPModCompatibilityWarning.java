3f95ef4b8e8c82bb6580097389df89dd9c7bab5f
qouteall <qouteall@163.com>
2022-07-13 21:35:45 +0800

    change replay mod to maybe incompatible

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/compat/IPModCompatibilityWarning.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/compat/IPModCompatibilityWarning.java
@@ -27,7 +27,6 @@ public class IPModCompatibilityWarning {
         new ModInfo("altoclef", "Altoclef"),
 //        new ModInfo("bitsandchisels", "Bits & Chisels"),
         new ModInfo("tweakeroo", "Tweakeroo"),
-        new ModInfo("replaymod", "Replay Mod"),
         new ModInfo("twilightforest", "Twilight Forest"),
         new ModInfo("c2me", "C2ME")
     );
@@ -41,6 +40,7 @@ public class IPModCompatibilityWarning {
         new ModInfo("requiem", "Requiem"),
         new ModInfo("vmp", "VMP"),
         new ModInfo("modern_industrialization", "Modern Industrialization"),
+        new ModInfo("replaymod", "Replay Mod"),
         new ModInfo("create", "Create")
     );
     
