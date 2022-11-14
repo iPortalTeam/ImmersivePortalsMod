4346fde9223f68a7d99cb9e09ca839b7fa87c271
qouteall <qouteall@163.com>
2022-11-14 22:01:56 +0800

    Remove dubious things

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/miscellaneous/DubiousThings.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/miscellaneous/DubiousThings.java
@@ -7,41 +7,13 @@ import net.minecraft.world.entity.Entity;
 import qouteall.imm_ptl.core.IPGlobal;
 import qouteall.q_misc_util.Helper;
 
-// temporary work around for bugs
 public class DubiousThings {
     public static void init() {
         IPGlobal.postClientTickSignal.connect(DubiousThings::tick);
     }
     
     private static void tick() {
-        ClientLevel world = Minecraft.getInstance().level;
-        LocalPlayer player = Minecraft.getInstance().player;
-        if (world == null) {
-            return;
-        }
-        if (player == null) {
-            return;
-        }
-        if (world.getGameTime() % 233 == 34) {
-            checkClientPlayerState();
-        }
+        // things removed
     }
     
-    private static void checkClientPlayerState() {
-        Minecraft client = Minecraft.getInstance();
-        if (client.level != client.player.level) {
-            Helper.err("Player world abnormal");
-            //don't know how to fix it
-        }
-        if (!client.player.isRemoved()) {
-            Entity playerInWorld = client.level.getEntity(client.player.getId());
-            if (playerInWorld != client.player) {
-                Helper.err("Client Player Mismatch");
-                if (playerInWorld instanceof LocalPlayer) {
-                    client.player = ((LocalPlayer) playerInWorld);
-                    Helper.log("Force corrected");
-                }
-            }
-        }
-    }
 }
