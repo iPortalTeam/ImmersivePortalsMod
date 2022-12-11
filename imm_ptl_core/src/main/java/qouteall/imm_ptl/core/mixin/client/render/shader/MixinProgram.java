b776256ae0456df0f9e265fb88a04d29b4608c88
qouteall <qouteall@163.com>
2022-12-11 19:58:31 +0800

    Fix iris compatibility

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/mixin/client/render/shader/MixinProgram.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/mixin/client/render/shader/MixinProgram.java
@@ -6,51 +6,76 @@ import com.mojang.blaze3d.preprocessor.GlslPreprocessor;
 import com.mojang.blaze3d.shaders.Program;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import qouteall.imm_ptl.core.miscellaneous.IPVanillaCopy;
 import qouteall.imm_ptl.core.render.ShaderCodeTransformation;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
-// 800 priority to avoid clash with iris
-@Mixin(value = Program.class, priority = 800)
+@Mixin(value = Program.class)
 public class MixinProgram {
-    /**
-     * @author qouteall
-     * @reason make the logic clear (Iris redirects it)
-     */
-    @IPVanillaCopy
-    @Overwrite
-    public static int compileShaderInternal(
-        Program.Type type, String name, InputStream stream,
-        String domain, GlslPreprocessor loader
+    // The redirect uses method arguments.
+    // Iris also injects that method and uses local capture, so cannot overwrite.
+    private static final ThreadLocal<Program.Type> ip_programType = new ThreadLocal<>();
+    private static final ThreadLocal<String> ip_programName = new ThreadLocal<>();
+    
+    @Inject(
+        method = "compileShaderInternal",
+        at = @At("HEAD")
+    )
+    private static void onBeginCompileShaderInternal(
+        Program.Type type, String name, InputStream shaderData,
+        String sourceName, GlslPreprocessor preprocessor, CallbackInfoReturnable<Integer> cir
+    ) {
+        Validate.isTrue(ip_programType.get() == null);
+        Validate.isTrue(ip_programName.get() == null);
+        ip_programType.set(type);
+        ip_programName.set(name);
+    }
+    
+    @Inject(
+        method = "compileShaderInternal",
+        at = @At("RETURN")
+    )
+    private static void onEndCompileShaderInternal(
+        Program.Type type, String name, InputStream shaderData,
+        String sourceName, GlslPreprocessor preprocessor, CallbackInfoReturnable<Integer> cir
+    ) {
+        Validate.isTrue(ip_programType.get() == type);
+        Validate.isTrue(Objects.equals(ip_programName.get(), name));
+        ip_programType.set(null);
+        ip_programName.set(null);
+    }
+    
+    @Redirect(
+        method = "compileShaderInternal",
+        at = @At(
+            value = "INVOKE",
+            target = "Lorg/apache/commons/io/IOUtils;toString(Ljava/io/InputStream;Ljava/nio/charset/Charset;)Ljava/lang/String;"
+        )
+    )
+    private static String redirectReadShaderSource(
+        InputStream inputStream, Charset charset
     ) throws IOException {
-        String shaderCode = IOUtils.toString(stream, StandardCharsets.UTF_8);
-        if (shaderCode == null) {
-            throw new IOException("Could not load program " + type.getName());
-        }
+        String shaderCode = IOUtils.toString(inputStream, charset);
+        Program.Type type = ip_programType.get();
+        String name = ip_programName.get();
+        Validate.notNull(type);
+        Validate.notNull(name);
         
         String transformedShaderCode =
             ShaderCodeTransformation.transform(type, name, shaderCode);
         
-        int shaderId = GlStateManager.glCreateShader(type.getGlType());
-        
-        GlStateManager.glShaderSource(shaderId, loader.process(transformedShaderCode));
-        
-        GlStateManager.glCompileShader(shaderId);
-        
-        if (GlStateManager.glGetShaderi(shaderId, 35713) == 0) {
-            String errorMessage = StringUtils.trim(
-                GlStateManager.glGetShaderInfoLog(shaderId, 32768)
-            );
-            throw new IOException(
-                "Couldn't compile " + type.getName() +
-                    " program (" + domain + ", " + name + ") : " + errorMessage
-            );
-        }
-        return shaderId;
+        return transformedShaderCode;
     }
 }
