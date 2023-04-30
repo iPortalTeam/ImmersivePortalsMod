d47439c15f8f8ba82e3d68c9f0a540327922b9dd
qouteall <qouteall@163.com>
2023-04-30 11:44:25 +0800

    Fix iris cutout not being clipped

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/compat/mixin/MixinIrisSodiumSodiumTerrainPipeline.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/compat/mixin/MixinIrisSodiumSodiumTerrainPipeline.java
@@ -1,11 +1,14 @@
 package qouteall.imm_ptl.core.compat.mixin;
 
 import com.mojang.blaze3d.shaders.Program;
+import me.jellysquid.mods.sodium.client.render.vertex.type.ChunkVertexType;
 import net.coderbot.iris.pipeline.SodiumTerrainPipeline;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import qouteall.imm_ptl.core.render.ShaderCodeTransformation;
 
@@ -14,27 +17,40 @@ import java.util.Optional;
 @Pseudo
 @Mixin(value = SodiumTerrainPipeline.class, remap = false)
 public class MixinIrisSodiumSodiumTerrainPipeline {
-    @Inject(method = "getTerrainSolidVertexShaderSource", at = @At("RETURN"), cancellable = true)
-    private void onGetTerrainVertexShaderSource(CallbackInfoReturnable<Optional<String>> cir) {
-        Optional<String> original = cir.getReturnValue();
-        cir.setReturnValue(original.map(code ->
+    @Shadow
+    Optional<String> terrainSolidVertex;
+    
+    @Shadow
+    Optional<String> terrainCutoutVertex;
+    
+    @Shadow
+    Optional<String> translucentVertex;
+    
+    @Inject(
+        method = "patchShaders",
+        at = @At("RETURN")
+    )
+    private void onPathShaderEnds(ChunkVertexType par1, CallbackInfo ci) {
+        terrainSolidVertex = terrainSolidVertex.map(code ->
             ShaderCodeTransformation.transform(
                 Program.Type.VERTEX,
                 "iris_sodium_terrain_vertex",
                 code
             )
-        ));
-    }
-    
-    @Inject(method = "getTranslucentVertexShaderSource", at = @At("RETURN"), cancellable = true)
-    private void onGetTranslucentVertexShaderSource(CallbackInfoReturnable<Optional<String>> cir) {
-        Optional<String> original = cir.getReturnValue();
-        cir.setReturnValue(original.map(code ->
+        );
+        terrainCutoutVertex = terrainCutoutVertex.map(code ->
             ShaderCodeTransformation.transform(
                 Program.Type.VERTEX,
-                "iris_sodium_translucent_vertex",
+                "iris_sodium_terrain_vertex",
                 code
             )
-        ));
+        );
+        translucentVertex = translucentVertex.map(code ->
+            ShaderCodeTransformation.transform(
+                Program.Type.VERTEX,
+                "iris_sodium_terrain_vertex",
+                code
+            )
+        );
     }
 }
