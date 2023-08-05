6c79a884a6176e7e099ebfb5549d12b026944242
qouteall <qouteall@163.com>
2023-08-05 13:00:56 +0800

    Optimize client chunk map by eliminating synchronization of chunk read from main thread

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/chunk_loading/ImmPtlClientChunkMap.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/chunk_loading/ImmPtlClientChunkMap.java
@@ -1,9 +1,10 @@
 package qouteall.imm_ptl.core.chunk_loading;
 
-import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientChunkCache;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.SectionPos;
@@ -15,11 +16,13 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.chunk.ChunkStatus;
 import net.minecraft.world.level.chunk.LevelChunk;
+import org.apache.commons.lang3.Validate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import qouteall.imm_ptl.core.CHelper;
 import qouteall.imm_ptl.core.ClientWorldLoader;
 import qouteall.imm_ptl.core.McHelper;
+import qouteall.imm_ptl.core.ducks.IEMinecraftClient;
 import qouteall.imm_ptl.core.miscellaneous.IPVanillaCopy;
 import qouteall.imm_ptl.core.platform_specific.O_O;
 import qouteall.q_misc_util.my_util.SignalArged;
@@ -27,75 +30,106 @@ import qouteall.q_misc_util.my_util.SignalArged;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Vanilla use a 2D array to store the chunk references on client and cannot store the chunks that are far from player.
- * This use a map to store the chunk references.
+ * This use map to store the chunk references, to eliminate such limitation.
+ * (Two maps, one for main thread and one for other threads)
  */
 @Environment(EnvType.CLIENT)
 @IPVanillaCopy
 public class ImmPtlClientChunkMap extends ClientChunkCache {
     private static final Logger LOGGER = LogManager.getLogger();
     
-    protected final Long2ObjectLinkedOpenHashMap<LevelChunk> chunkMap =
-        new Long2ObjectLinkedOpenHashMap<>();
+    // the most chunk accesses are from the main thread,
+    // so we use two maps to reduce synchronization.
+    // the main thread reads this map, without synchronization
+    protected final Long2ObjectOpenHashMap<LevelChunk> chunkMapForMainThread =
+        new Long2ObjectOpenHashMap<>();
+    // other threads read this map, with synchronization
+    protected final Long2ObjectOpenHashMap<LevelChunk> chunkMapForOtherThreads =
+        new Long2ObjectOpenHashMap<>();
+    
+    public final Thread mainThread;
     
     public static final SignalArged<LevelChunk> clientChunkLoadSignal = new SignalArged<>();
     public static final SignalArged<LevelChunk> clientChunkUnloadSignal = new SignalArged<>();
     
     public ImmPtlClientChunkMap(ClientLevel clientWorld, int loadDistance) {
-        super(clientWorld, 1); // the chunk array is unused. make it small by passing 1 as load distance to super constructor
+        super(clientWorld, 1);
+        // the chunk array is unused. make it small by passing 1 as load distance to super constructor
+        
+        mainThread = ((IEMinecraftClient) Minecraft.getInstance()).ip_getRunningThread();
     }
     
     @Override
     public void drop(int x, int z) {
-        synchronized (chunkMap) {
-            
-            ChunkPos chunkPos = new ChunkPos(x, z);
-            LevelChunk chunk = chunkMap.get(chunkPos.toLong());
-            if (isValidChunk(chunk, x, z)) {
+        Validate.isTrue(Thread.currentThread() == mainThread);
+        
+        ChunkPos chunkPos = new ChunkPos(x, z);
+        LevelChunk chunk = chunkMapForMainThread.get(chunkPos.toLong());
+        if (chunk != null) {
+            modifyChunkMap(chunkMap -> {
                 chunkMap.remove(chunkPos.toLong());
-                O_O.postClientChunkUnloadEvent(chunk);
-                this.level.unload(chunk);
-                clientChunkUnloadSignal.emit(chunk);
+            });
+            
+            O_O.postClientChunkUnloadEvent(chunk);
+            this.level.unload(chunk);
+            clientChunkUnloadSignal.emit(chunk);
+        }
+    }
+    
+    public <T> T readChunkMap(Function<Long2ObjectOpenHashMap<LevelChunk>, T> func) {
+        if (Thread.currentThread() == mainThread) {
+            return func.apply(chunkMapForMainThread);
+        }
+        else {
+            synchronized (chunkMapForOtherThreads) {
+                return func.apply(chunkMapForOtherThreads);
             }
+        }
+    }
+    
+    public void modifyChunkMap(Consumer<Long2ObjectOpenHashMap<LevelChunk>> func) {
+        Validate.isTrue(Thread.currentThread() == mainThread);
+        func.accept(chunkMapForMainThread);
+        synchronized (chunkMapForOtherThreads) {
+            func.accept(chunkMapForOtherThreads);
         }
     }
     
     @Override
     public LevelChunk getChunk(int x, int z, ChunkStatus chunkStatus, boolean create) {
-        // the profiler shows that this is not a hot spot
-        synchronized (chunkMap) {
+        return readChunkMap(chunkMap -> {
             LevelChunk chunk = chunkMap.get(ChunkPos.asLong(x, z));
-            if (isValidChunk(chunk, x, z)) {
+            if (chunk != null) {
                 return chunk;
             }
             
             return create ? this.emptyChunk : null;
-        }
+        });
     }
     
     public boolean isChunkLoaded(int x, int z) {
-        synchronized (chunkMap) {
+        return readChunkMap(chunkMap -> {
             return chunkMap.containsKey(ChunkPos.asLong(x, z));
-        }
+        });
     }
     
     @Override
     public void replaceBiomes(int x, int z, FriendlyByteBuf friendlyByteBuf) {
+        Validate.isTrue(Thread.currentThread() == mainThread);
+        
         long chunkPosLong = ChunkPos.asLong(x, z);
         
-        LevelChunk worldChunk;
-        
-        synchronized (chunkMap) {
-            worldChunk = chunkMap.get(chunkPosLong);
-            ChunkPos chunkPos = new ChunkPos(x, z);
-            if (!isValidChunk(worldChunk, x, z)) {
-                LOGGER.error("Trying to replace biomes for missing chunk {} {}", x, z);
-            }
-            else {
-                worldChunk.replaceBiomes(friendlyByteBuf);
-            }
+        LevelChunk worldChunk = chunkMapForMainThread.get(chunkPosLong);
+        ChunkPos chunkPos = new ChunkPos(x, z);
+        if (worldChunk == null) {
+            LOGGER.error("Trying to replace biomes for missing chunk {} {}", x, z);
+        }
+        else {
+            worldChunk.replaceBiomes(friendlyByteBuf);
         }
     }
     
@@ -105,21 +139,22 @@ public class ImmPtlClientChunkMap extends ClientChunkCache {
         FriendlyByteBuf buf, CompoundTag nbt,
         Consumer<ClientboundLevelChunkPacketData.BlockEntityTagOutput> consumer
     ) {
+        Validate.isTrue(Thread.currentThread() == mainThread);
         long chunkPosLong = ChunkPos.asLong(x, z);
         
-        LevelChunk worldChunk;
-        
-        synchronized (chunkMap) {
-            worldChunk = chunkMap.get(chunkPosLong);
-            ChunkPos chunkPos = new ChunkPos(x, z);
-            if (!isValidChunk(worldChunk, x, z)) {
-                worldChunk = new LevelChunk(this.level, chunkPos);
-                loadChunkDataFromPacket(buf, nbt, worldChunk, consumer);
-                chunkMap.put(chunkPosLong, worldChunk);
-            }
-            else {
-                loadChunkDataFromPacket(buf, nbt, worldChunk, consumer);
-            }
+        LevelChunk worldChunk = chunkMapForMainThread.get(chunkPosLong);
+        ChunkPos chunkPos = new ChunkPos(x, z);
+        if (worldChunk == null) {
+            worldChunk = new LevelChunk(this.level, chunkPos);
+            loadChunkDataFromPacket(buf, nbt, worldChunk, consumer);
+            
+            LevelChunk worldChunkToPut = worldChunk; // lambda can only capture effectively final variables
+            modifyChunkMap(chunkMap -> {
+                chunkMap.put(chunkPosLong, worldChunkToPut);
+            });
+        }
+        else {
+            loadChunkDataFromPacket(buf, nbt, worldChunk, consumer);
         }
         
         this.level.onChunkLoaded(new ChunkPos(x, z));
@@ -166,19 +201,19 @@ public class ImmPtlClientChunkMap extends ClientChunkCache {
     }
     
     public List<LevelChunk> getCopiedChunkList() {
-        synchronized (chunkMap) {
+        return readChunkMap((chunkMap) -> {
             return Arrays.asList(chunkMap.values().toArray(new LevelChunk[0]));
-        }
+        });
     }
     
     @Override
     public void updateViewCenter(int x, int z) {
-        //do nothing
+        // do nothing
     }
     
     @Override
     public void updateViewRadius(int r) {
-        //do nothing
+        // do nothing
     }
     
     @Override
@@ -188,30 +223,15 @@ public class ImmPtlClientChunkMap extends ClientChunkCache {
     
     @Override
     public int getLoadedChunksCount() {
-        synchronized (chunkMap) {
+        return readChunkMap(chunkMap -> {
             return chunkMap.size();
-        }
+        });
     }
     
     @Override
     public void onLightUpdate(LightLayer lightType, SectionPos chunkSectionPos) {
-        ClientWorldLoader.getWorldRenderer(
-            level.dimension()
-        ).setSectionDirty(
-            chunkSectionPos.x(),
-            chunkSectionPos.y(),
-            chunkSectionPos.z()
-        );
-    }
-    
-    protected static boolean isValidChunk(LevelChunk worldChunk, int x, int z) {
-        if (worldChunk == null) {
-            return false;
-        }
-        else {
-            ChunkPos chunkPos = worldChunk.getPos();
-            return chunkPos.x == x && chunkPos.z == z;
-        }
+        ClientWorldLoader.getWorldRenderer(level.dimension())
+            .setSectionDirty(chunkSectionPos.x(), chunkSectionPos.y(), chunkSectionPos.z());
     }
     
 }
