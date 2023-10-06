1e8d7b2805504e7c88bcb6799256f9a5902f993a
qouteall <qouteall@163.com>
2023-10-06 18:16:05 +0800

    fix crash with cardinal comp

--- imm_ptl_core/src/main/java/qouteall/imm_ptl/core/compat/mixin/cardinal_comp/MixinCardinalCompComponentKey.java
+++ imm_ptl_core/src/main/java/qouteall/imm_ptl/core/compat/mixin/cardinal_comp/MixinCardinalCompComponentKey.java
@@ -4,6 +4,7 @@ import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentProvider;
 import dev.onyxstudios.cca.api.v3.component.sync.ComponentPacketWriter;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import qouteall.imm_ptl.core.network.PacketRedirection;
 
-@Mixin(targets = "dev.onyxstudios.cca.api.v3.component.ComponentKey")
+@Mixin(ComponentKey.class)
 public class MixinCardinalCompComponentKey {
     // redirect the entity sync packet
     @SuppressWarnings({"unchecked", "rawtypes"})
@@ -20,20 +21,21 @@ public class MixinCardinalCompComponentKey {
         method = "Ldev/onyxstudios/cca/api/v3/component/ComponentKey;syncWith(Lnet/minecraft/server/level/ServerPlayer;Ldev/onyxstudios/cca/api/v3/component/ComponentProvider;Ldev/onyxstudios/cca/api/v3/component/sync/ComponentPacketWriter;Ldev/onyxstudios/cca/api/v3/component/sync/PlayerSyncPredicate;)V",
         at = @At(
             value = "INVOKE",
-            target = "Ldev/onyxstudios/cca/api/v3/component/ComponentProvider;toComponentPacket(Ldev/onyxstudios/cca/api/v3/component/ComponentKey;Ldev/onyxstudios/cca/api/v3/component/sync/ComponentPacketWriter;Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/network/protocol/game/ClientboundCustomPayloadPacket;"
+            target = "Ldev/onyxstudios/cca/api/v3/component/ComponentProvider;toComponentPacket(Ldev/onyxstudios/cca/api/v3/component/ComponentKey;Ldev/onyxstudios/cca/api/v3/component/sync/ComponentPacketWriter;Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/network/protocol/common/ClientboundCustomPayloadPacket;"
         )
     )
-    private Packet<?> redirectPacket(
+    private ClientboundCustomPayloadPacket redirectPacket(
         ComponentProvider instance, ComponentKey<?> key, ComponentPacketWriter writer, ServerPlayer recipient
     ) {
-        Packet<ClientGamePacketListener> packet =
-            (Packet<ClientGamePacketListener>) (Packet) instance.toComponentPacket(key, writer, recipient);
+        ClientboundCustomPayloadPacket packet =
+            instance.toComponentPacket(key, writer, recipient);
         if (instance instanceof Entity entity) {
-            packet = PacketRedirection.createRedirectedMessage(
+            var redirected = PacketRedirection.createRedirectedMessage(
                 entity.getServer(),
                 entity.level().dimension(),
                 (Packet<ClientGamePacketListener>) (Packet) packet
             );
+            packet = (ClientboundCustomPayloadPacket) (Packet) redirected;
         }
         return packet;
     }
