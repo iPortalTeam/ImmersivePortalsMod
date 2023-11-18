82cf890099fedf3bd02af1d4dfebb17a7ed17c41
qouteall <qouteall@163.com>
2023-11-18 09:58:19 +0800

    sync translation

---
+++ imm_ptl_core/sync-crowdin.bat
@@ -0,0 +1,6 @@
+:: need to download https://github.com/gbl/CrowdinTranslate/releases and put into this folder
+:: build on crowdin before using this
+
+java -jar crowdin-translate-1.2.jar immersive-portals-mod immersive_portals
+
+PAUSE
