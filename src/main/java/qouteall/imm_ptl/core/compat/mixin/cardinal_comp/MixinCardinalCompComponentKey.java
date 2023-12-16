f457b9d60e961e2ca7d519a5e2d28493fcaffb2e
qouteall <qouteall@163.com>
2023-12-16 15:47:41 +0800

    avoid compile dependency to cardinal components

--- src/main/java/qouteall/imm_ptl/core/compat/mixin/cardinal_comp/MixinCardinalCompComponentKey.java
+++ src/main/java/qouteall/imm_ptl/core/compat/mixin/cardinal_comp/MixinCardinalCompComponentKey.java
@@ -1,23 +1,26 @@
 package qouteall.imm_ptl.core.compat.mixin.cardinal_comp;
 
-import dev.onyxstudios.cca.api.v3.component.ComponentKey;
-import dev.onyxstudios.cca.api.v3.component.ComponentProvider;
-import dev.onyxstudios.cca.api.v3.component.sync.ComponentPacketWriter;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import qouteall.imm_ptl.core.network.PacketRedirection;
 
-@Mixin(ComponentKey.class)
+@Pseudo
+@Mixin(targets = "dev.onyxstudios.cca.api.v3.component.ComponentKey")
 public class MixinCardinalCompComponentKey {
     // redirect the entity sync packet
     @SuppressWarnings({"unchecked", "rawtypes"})
-    @Redirect(
+    @WrapOperation(
         method = "Ldev/onyxstudios/cca/api/v3/component/ComponentKey;syncWith(Lnet/minecraft/server/level/ServerPlayer;Ldev/onyxstudios/cca/api/v3/component/ComponentProvider;Ldev/onyxstudios/cca/api/v3/component/sync/ComponentPacketWriter;Ldev/onyxstudios/cca/api/v3/component/sync/PlayerSyncPredicate;)V",
         at = @At(
             value = "INVOKE",
@@ -25,18 +28,29 @@ public class MixinCardinalCompComponentKey {
         )
     )
     private ClientboundCustomPayloadPacket redirectPacket(
-        ComponentProvider instance, ComponentKey<?> key, ComponentPacketWriter writer, ServerPlayer recipient
+        @Coerce Object instance, @Coerce Object key,
+        @Coerce Object writer, ServerPlayer recipient,
+        Operation<Packet<?>> operation
     ) {
-        ClientboundCustomPayloadPacket packet =
-            instance.toComponentPacket(key, writer, recipient);
+        Packet<?> packet = operation.call(instance, key, writer, recipient);
+        
         if (instance instanceof Entity entity) {
             var redirected = PacketRedirection.createRedirectedMessage(
                 entity.getServer(),
                 entity.level().dimension(),
                 (Packet<ClientGamePacketListener>) (Packet) packet
             );
-            packet = (ClientboundCustomPayloadPacket) (Packet) redirected;
+            packet = redirected;
         }
-        return packet;
+        else if (instance instanceof BlockEntity blockEntity) {
+            var redirected = PacketRedirection.createRedirectedMessage(
+                blockEntity.getLevel().getServer(),
+                blockEntity.getLevel().dimension(),
+                (Packet<ClientGamePacketListener>) (Packet) packet
+            );
+            packet = redirected;
+        }
+        
+        return (ClientboundCustomPayloadPacket) packet;
     }
 }
